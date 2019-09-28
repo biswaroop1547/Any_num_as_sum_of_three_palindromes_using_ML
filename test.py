98eb66d02748b85ae7541137144ad85a811b16a2
biswaroop1547 <biswaroop08@gmail.com>
2019-09-28 17:47:08 +0530

    made the script ready to make dataset

--- test.py
+++ test.py
@@ -6,32 +6,43 @@ import urllib.request
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import csv
 
 URL = 'http://www.rnta.eu/cgi-bin/three_palindromes/pal3.py'
 
 driver = webdriver.Chrome()
-num_list = []
-for i in range(1, 100):
+dataset = []
+for i in range(1324, 1330):
 
     driver.get(URL)
     writing_space = driver.find_element_by_id('number')
     writing_space.clear()
     writing_space.send_keys(i)
     writing_space.submit()
-    nums = []
+    nums = {"Number": i, "First": 0, "Second": 0, "Third": 0}
     html = driver.page_source
     soup = BeautifulSoup(html, features="html.parser")
+    count = 0
     for tag in soup.find_all('font'):
 
         if "is a palindrome" in tag.text:
-            continue
+            nums["First"] = i
+            break
 
         if "-" in tag.text:
             break
 
-        nums.append(tag.text)
-    num_list.append(nums)
-for i in num_list:
-    print(i)
+        if count == 0:
+            nums["First"] = tag.text
 
-print("the length is ", len(num_list))
+        elif count == 1:
+            nums["Second"] = tag.text
+
+        elif count == 2:
+            nums["Third"] = tag.text
+
+        count += 1
+    dataset.append(nums)
+
+
+print(dataset)
